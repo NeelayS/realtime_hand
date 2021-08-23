9c98b3c1ef129648cc1f54fa44665b2fc83b2499
NeelayS <shahnh19@gmail.com>
2021-08-24 02:14:13 +0530

    added custom ICNet

--- segmentation/models/blocks.py
+++ segmentation/models/blocks.py
@@ -71,3 +71,49 @@ class Bottleneck(nn.Module):
         out = self.relu_inplace(out)
 
         return out
+
+
+class ConvBnRelu(nn.Module):
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        ksize,
+        stride=1,
+        pad=0,
+        dilation=1,
+        groups=1,
+        has_bn=True,
+        norm_layer=nn.BatchNorm2d,
+        bn_eps=1e-5,
+        has_relu=True,
+        inplace=True,
+        has_bias=False,
+    ):
+        super(ConvBnRelu, self).__init__()
+
+        self.conv = nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=ksize,
+            stride=stride,
+            padding=pad,
+            dilation=dilation,
+            groups=groups,
+            bias=has_bias,
+        )
+        self.has_bn = has_bn
+        if self.has_bn:
+            self.bn = norm_layer(out_planes, eps=bn_eps)
+        self.has_relu = has_relu
+        if self.has_relu:
+            self.relu = nn.ReLU(inplace=inplace)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.has_bn:
+            x = self.bn(x)
+        if self.has_relu:
+            x = self.relu(x)
+
+        return x
