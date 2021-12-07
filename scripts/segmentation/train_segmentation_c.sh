370437c5ffd62ea8184c593a7b17caf7186f8f64
Neelay Shah <shahnh19@gmail.com>
2021-12-07 20:21:18 +0100

    Modified script

--- scripts/segmentation/train_segmentation_c.sh
+++ scripts/segmentation/train_segmentation_c.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH -o ../../outs/segmentation/training/BiSeNet.%j.out
 #SBATCH -e ../../errs/segmentation/training/BiSeNet.%j.err
-#SBATCH -t 30:00:00
+#SBATCH -t 36:00:00
 #SBATCH --mem=24G
 #SBATCH --gres=gpu:V100:1
 
@@ -12,11 +12,11 @@
 
 train_cfg="configs/segmentation/custom_loss_trainer.yaml"
 model="BiSeNet"
-img_dir="../sub_imgs" #"data/segmentation/Ego2Hands/train_imgs/" 
+img_dir="../sub_imgs" # "data/segmentation/Ego2Hands/train_imgs/" 
 bg_dir="data/segmentation/Ego2Hands/bg_imgs" 
-log_dir="logs/segmentation/BiSeNet/run8"
-ckpt_dir="ckpts/segmentation/BiSeNet/run8"
-epochs=15
+log_dir="logs/segmentation/BiSeNet/run13"
+ckpt_dir="ckpts/segmentation/BiSeNet/run13"
+epochs=20
 device="0"
 
 resume_ckpt="ckpts/segmentation/BiSeNet/BiSeNet_epoch_10.pth"
